cdb043a2eef5fcf891ac67695495f36d20cb8d8a
joroVN <joroarh@gmail.com>
2023-09-24 22:14:17 +0300

    Basic validation

---
+++ src/main/java/com/softuni/mobilele/web/OfferController.java
@@ -0,0 +1,18 @@
+package com.softuni.mobilele.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OfferController {
+
+    @GetMapping("offers/all")
+    public String allOffers() {
+        return "offers";
+    }
+
+    @GetMapping("offers/add")
+    public String addOffers() {
+        return "offer-add";
+    }
+}
