fcc67b1b4bed58ac5b08e7d2fdb0b78f3d0f203f
joroVN <joroarh@gmail.com>
2023-11-11 23:21:16 +0200

    Scheduler TODO

---
+++ src/main/java/com/softuni/mobilele/scheduler/ActivationTokenCleanUpScheduler.java
@@ -0,0 +1,14 @@
+package com.softuni.mobilele.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActivationTokenCleanUpScheduler {
+
+    //TODO
+    @Scheduled(cron = "0 5 * * * ?")
+    public void cleanUpTokens() {
+        //TODO - call the user service...
+    }
+}
